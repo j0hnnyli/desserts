edad82df24bec154bb62b667edf2a18c19dfb18c
Johnny Li <130239183+j0hnnyli@users.noreply.github.com>
2025-03-11 00:43:29 -0400

    Update : Removed Used Images and Components  (#4)
    * showcase and about page developed
    
    * added section animation transition and restructured file tree
    
    * created a dessert sections
    
    * removed unused images and components

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${crimsonPro.variable} antialiased  text-[var(--secondary-color)] bg-black`}
       >
-        {/* <Navbar /> */}
         <main>
           {children}
         </main>
